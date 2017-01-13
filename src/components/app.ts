e86817480ebbf6bf85f9dac5dc83cfcfef60bd57
Vogete <fraknoiz@gmail.com>
2017-01-13 14:46:29 +0100

    Import from simple txt files added, imported text is formatted to objects

--- src/components/app.ts
+++ src/components/app.ts
@@ -3,25 +3,37 @@
  */
 class Word {
     
-    private _word : string;
-    public get word() : string {
-        return this._word;
-    }
-    public set word(v : string) {
-        this._word = v;
-    }
+    // private _word : string;
+    // public get word() : string {
+    //     return this._word;
+    // }
+    // public set word(v : string) {
+    //     this._word = v;
+    // }
     
-    private _translations : [Translation];
-    public get translations() : [Translation] {
-        return this._translations;
-    }
-    public set translations(v : [Translation]) {
-        this._translations = v;
-    }
+    // private _translations : [Translation];
+    // public get translations() : [Translation] {
+    //     return this._translations;
+    // }
+    // public set translations(v : [Translation]) {
+    //     this._translations = v;
+    // }
+    
+    // private _wordLanguage : string;
+    // public get wordLanguage() : string {
+    //     return this._wordLanguage;
+    // }
+    // public set wordLanguage(v : string) {
+    //     this._wordLanguage = v;
+    // }
+
+    public Word: string;
+    public Translations : [Translation];
+    public WordLanguage : string;
 
     constructor(word: string, translations?: [Translation]) {
-        this._word = word;
-        this._translations = translations;
+        this.Word = word;
+        this.Translations = translations;
     }
 
 }
@@ -31,31 +43,70 @@ class Word {
  */
 class Translation {
     
-    private _word : string;
-    public get word() : string {
-        return this._word;
-    }
-    public set word(v : string) {
-        this._word = v;
-    }
+    // private _word : string;
+    // public get word() : string {
+    //     return this._word;
+    // }
+    // public set word(v : string) {
+    //     this._word = v;
+    // }
     
-    
-    private _language : string;
-    public get language() : string {
-        return this._language;
-    }
-    public set language(v : string) {
-        this._language = v;
-    }
-    
+    // private _translationLanguage : string;
+    // public get language() : string {
+    //     return this._translationLanguage;
+    // }
+    // public set language(v : string) {
+    //     this._translationLanguage = v;
+    // }
 
-    constructor(word: string, language: string) {
-        this._word = word;
-        this._language = language;
+    public Word : string;
+    public TranslationLanguage : string;    
+
+    constructor(word: string, language?: string) {
+        this.Word = word;
+        this.TranslationLanguage = language;
     }
 }
 
-let translation1 = new Translation("Hello", "English");
+// loading files
+window.onload = function() {
+		var fileInput = <HTMLInputElement>document.getElementById('inp-wordList');
+		var fileDisplayArea = document.getElementById('fileDisplayArea');
 
-let MyWord = new Word("Sziaaaaaa", [translation1]);
-console.log(MyWord);
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0];
+			var textType = /text.*/;
+
+			if (file.type.match(textType)) {
+				var reader = new FileReader();
+
+				reader.onload = function(e) {
+					// fileDisplayArea.innerText = reader.result;
+                    convertFromTxt(reader.result);                    
+				}
+
+				reader.readAsText(file);	
+			} else {
+				fileDisplayArea.innerText = "File not supported!"
+			}
+		});
+}
+
+
+let WordList: Word[] = [];
+
+
+function convertFromTxt(wordFile:string) {
+
+    let lines = wordFile.split("\r\n");
+
+    for (var i = 0; i < lines.length; i++) {
+        let line = lines[i].split("  ");
+        
+        WordList.push( new Word( line[0], [ new Translation(line[1]) ] ) );
+
+    }
+
+    console.log(WordList);
+    
+}
