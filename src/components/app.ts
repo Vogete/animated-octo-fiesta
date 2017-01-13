8427a598d6945b9e6d38c35c0650a7e2c2dd8d71
Vogete <fraknoiz@gmail.com>
2017-01-13 20:52:29 +0100

    refactoring

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,112 +1,74 @@
-/**
- * This is a class for the indiviual words.
- */
-class Word {
-    
-    // private _word : string;
-    // public get word() : string {
-    //     return this._word;
-    // }
-    // public set word(v : string) {
-    //     this._word = v;
-    // }
-    
-    // private _translations : [Translation];
-    // public get translations() : [Translation] {
-    //     return this._translations;
-    // }
-    // public set translations(v : [Translation]) {
-    //     this._translations = v;
-    // }
-    
-    // private _wordLanguage : string;
-    // public get wordLanguage() : string {
-    //     return this._wordLanguage;
-    // }
-    // public set wordLanguage(v : string) {
-    //     this._wordLanguage = v;
-    // }
+import {Word} from "./models";
+import {Translation} from "./models";
 
-    public Word: string;
-    public Translations : [Translation];
-    public WordLanguage : string;
-
-    constructor(word: string, translations?: [Translation]) {
-        this.Word = word;
-        this.Translations = translations;
-    }
-
-}
-
-/**
- * A translation for a word. It contains the translation and its language.
- */
-class Translation {
-    
-    // private _word : string;
-    // public get word() : string {
-    //     return this._word;
-    // }
-    // public set word(v : string) {
-    //     this._word = v;
-    // }
-    
-    // private _translationLanguage : string;
-    // public get language() : string {
-    //     return this._translationLanguage;
-    // }
-    // public set language(v : string) {
-    //     this._translationLanguage = v;
-    // }
-
-    public Word : string;
-    public TranslationLanguage : string;    
-
-    constructor(word: string, language?: string) {
-        this.Word = word;
-        this.TranslationLanguage = language;
-    }
-}
+let WordList : Word[] = [];
+let wordSeparator : string = "  ";
+let lineSeparator : string = "\r\n";
 
 // loading files
 window.onload = function() {
-		var fileInput = <HTMLInputElement>document.getElementById('inp-wordList');
-		var fileDisplayArea = document.getElementById('fileDisplayArea');
+    let fileInput = <HTMLInputElement>document.getElementById('inp-wordList');
+    let fileDisplayArea = document.getElementById('fileDisplayArea');
 
-		fileInput.addEventListener('change', function(e) {
-			var file = fileInput.files[0];
-			var textType = /text.*/;
+    fileInput.addEventListener('change', function(e) {
+        let file = fileInput.files[0];
+        let textType = /text.*/;
 
-			if (file.type.match(textType)) {
-				var reader = new FileReader();
+        if (file.type.match(textType)) {
+            let reader = new FileReader();
 
-				reader.onload = function(e) {
-					// fileDisplayArea.innerText = reader.result;
-                    convertFromTxt(reader.result);                    
-				}
+            reader.onload = function(e) {
+                // fileDisplayArea.innerText = reader.result;
+                WordList = convertFromTxt(reader.result);
+                
+                writeWordsInTable(WordList);
+            }
 
-				reader.readAsText(file);	
-			} else {
-				fileDisplayArea.innerText = "File not supported!"
-			}
-		});
+            reader.readAsText(file);            
+        } else {
+            // fileDisplayArea.innerText = "File not supported!";
+            console.log("Error, file not supported");
+            
+        }
+    });
 }
 
 
-let WordList: Word[] = [];
+function convertFromTxt(wordFile:string) : Word[] {
 
-
-function convertFromTxt(wordFile:string) {
-
-    let lines = wordFile.split("\r\n");
+    let lines = wordFile.split(lineSeparator);
+    let convertedWords: Word[] = [];
 
     for (var i = 0; i < lines.length; i++) {
-        let line = lines[i].split("  ");
-        
-        WordList.push( new Word( line[0], [ new Translation(line[1]) ] ) );
+        let line = lines[i].split(wordSeparator);
+        if (line[0].length > 0) {
+            // convertedWords.push( new Word( line[0], [ new Translation(line[1]) ] ) );            
+            convertedWords.push(createWord( line[0], [ line[1] ] ));
+        }
+    }
+    
+    return convertedWords;    
+}
 
+function writeWordsInTable(words: Word[]) {
+    console.log(words);
+    let appendHTML : string = "";
+    let tblWordlist =  <HTMLTableSectionElement>document.getElementById("tbl-wordList").getElementsByTagName("tbody")[0];
+    for (var i = 0; i < words.length; i++) {
+         appendHTML += `<tr><td>${words[i].Word}</td><td>${words[i].Translations[0].Word}</td></tr>`;
+                
+    }
+    tblWordlist.innerHTML = appendHTML;
+}
+
+function createWord(word:string, meanings:string[]) : Word {    
+    let _translations : Translation[] = [];
+
+    for (var i = 0; i < meanings.length; i++) {
+        _translations.push(new Translation(meanings[i]));
     }
 
-    console.log(WordList);
-    
+    let _newWord = new Word(word, _translations);
+
+    return _newWord;
 }
